3cd583fc1e3d0ae5a2f5e2a5f963020a5a4280c9
lh123cha <1491337293@qq.com>
2021-06-10 21:58:27 -0700

    waimai

--- flaskProject/WaiMai.sol
+++ flaskProject/WaiMai.sol
@@ -4,37 +4,34 @@ pragma solidity >=0.4.16 <0.9.0; // 编译指令，指明solidity版本，上面
 // 合约
 contract WaiMai {   
     // 玩家结构体
-    struct Student {
-        uint id;        // student id
-        string name;    // student's name
-        string sex;     // student's sex
-        uint age;       // student's age
-        string dept;    // student's dept
+    struct User {
+        address addr;       // address
+        uint id;        //id
+        string name;    // name
+        string tel;     // telephone
+        uint money;       // money
+        string dept;    // dept
     }
-    uint num;   // student数目
+
+    uint num;   // user数目
     uint[] ids; //id数组
-    Student[] students;   // 存储所有student的数据
-    address public headmaster;// 只有校长才能加学生
-   
-    
-    event Insert(uint id);
+    User[] users;   // 存储所有student的数据
+    mapping(address => uint) userId;  // 地址到id的映射
+    mapping(address => bool) isPlay;    // 判断某个地址（即账户）是否参与
+    //event Insert(uint id);
 
     constructor() {
         num = 0;
-        headmaster= msg.sender;
     }
 
-       
-    function insert(uint _id,string memory _name,string memory _sex,uint _age,string memory _dept) public {
-        require(
-            msg.sender == headmaster,
-            "Only headmaster can add students."
-        );
-        students.push(Student(_id, _name, _sex, _age, _dept));   
+    function insert_user(string memory _name,string memory _tel,string memory _dept) public {
+        require(!isPlay[msg.sender]);
+        users.push(User(msg.sender, num,_name, _tel, 100, _dept));
+        ids.push(num);
+        userId[msg.sender] = num;
         num += 1;
-        ids.push(_id);
-        uint id = _id;
-        emit Insert(id);
+        //uint id = _id;
+        //emit Insert(id);
     }
 
     function select_count() public view returns (uint n){
@@ -46,19 +43,16 @@ contract WaiMai {
         return ids;
     }
 
-    
-    function select_id(uint _id) public view returns (Student memory){
+    function select_id(uint _id) public view returns (User memory){
         require(num>0);
+        //require(isPlay[msg.sender]);
         for (uint i = 0; i < num; ++i) {
-            if (students[i].id == _id) {
-                return students[i];
+            if (users[i].id == _id) {
+                return users[i];
             }
         }
-        return Student(0, "init", "init", 0, "init");
+        return User(msg.sender, 0,"null", "null", 0, "null");
     }
-
-
-
 
 
 }
--- flaskProject/app.py
+++ flaskProject/app.py
@@ -9,8 +9,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-student_contract = compile_and_deploy("WaiMai")
-
+waimai_contract = compile_and_deploy("WaiMai")
 
 @app.route('/')
 def hello():
@@ -33,31 +32,15 @@ def add():
             data=request.get_data()
             print("data = %s"%data)
             json_data=json.loads(data.decode("utf-8"))
-            id = json_data.get("id")
-            name=json_data.get("name")
-            sex=json_data.get("sex")
-            age=json_data.get("age")
+            name = json_data.get("name")
+            tel=json_data.get("tel")
             dept=json_data.get("dept")
-            mystudent=(int(id),name,sex,int(age),dept)
-            print(mystudent)
-            tx_hash = student_contract.functions.insert(*mystudent).transact()
+            mywaimai=(name,tel,dept)
+            print(mywaimai)
+            eth.default_account = eth.accounts[1]
+            tx_hash = waimai_contract.functions.insert_user(*mywaimai).transact()
             tx_receipt = eth.waitForTransactionReceipt(tx_hash)
             return "<h1>insert success!</h1>"
-        # student = (1, "qk", "M", 22, "ECNU")
-        # student2 = (2, "lh", "M", 22, "ECNU")
-        # student3 = (3, "czr", "M", 22, "ECNU")
-        # student4 = (4, "yzk", "M", 22, "ECNU")
-        # # 构造函数通过rpc向以太坊发送该函数交易
-        # # *为解包操作，即传入的参数为: id, name, sex, age, dept
-        # tx_hash = student_contract.functions.insert(*student).transact()
-        # tx_receipt = eth.waitForTransactionReceipt(tx_hash)
-        # tx_hash2 = student_contract.functions.insert(*student2).transact()
-        # tx_receipt2 = eth.waitForTransactionReceipt(tx_hash2)
-        # tx_hash3 = student_contract.functions.insert(*student3).transact()
-        # tx_receipt3 = eth.waitForTransactionReceipt(tx_hash3)
-        # tx_hash4 = student_contract.functions.insert(*student4).transact()
-        # tx_receipt = eth.waitForTransactionReceipt(tx_hash4)
-        # return "<h1>insert success!</h1>"
     except:
         return "<h1>insert error!</h1>"
 
@@ -71,7 +54,7 @@ def query_process(key):
     key_int = int(key)
 
     try:
-        student = student_contract.functions.select_id(key_int).call()
+        student = waimai_contract.functions.select_id(key_int).call()
         if student is None:
             return "<h1>找不到该id对应的value</h1>"
         student = tuple(student)
