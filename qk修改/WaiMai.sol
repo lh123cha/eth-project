8157d8c4cef95c8d16057eaa78f72bbb5e4b5689
Kun Qian <qiankunhm@163.com>
2021-06-18 15:26:40 +0800

    qk添加三个！！！！函数
    qk添加三个！！！！函数，并在智能合约中添加三个函数，代码在qk修改文件夹下！！！

---
+++ qk修改/WaiMai.sol
@@ -0,0 +1,220 @@
+// SPDX-License-Identifier: GPL-3.0 
+pragma solidity >=0.4.16 <0.9.0; // 编译指令，指明solidity版本，上面一行是自由软件许可证
+
+// 合约
+contract WaiMai {   
+    // 玩家结构体
+    struct User {
+        address addr;       // address
+        uint id;        //id
+        string name;    // name
+        string tel;     // telephone
+        uint money;       // money
+        string dept;    // dept
+    }
+
+    //订单结构体
+    struct Deal{
+        uint id;       //id
+        string text;   //text
+        address send_user;  //senduser address
+        uint money;     //money
+        address receive_user;   //receiveuser address
+        bool is_finish;  //whether deal is finish
+        uint left_hour;  //hour time left
+        bool is_timeout;  //whether time is out
+        string tip; //tip of the deal
+
+    }
+
+    uint num;   // user数目
+    uint[] ids; //id数组
+    User[] users;   // 存储所有user的数据
+    mapping(address => uint) userId;  // 地址到id的映射
+    mapping(address => bool) isPlay;    // 判断某个地址（即账户）是否参与
+    //event Insert(uint id);
+    uint deal_num; //deal数目
+    uint[] deal_ids;//订单号数组
+    Deal[] deals;//储存所有订单的数据
+    
+    User current_user = User(address(0x0), 0,"null", "null", 0, "null");
+    User deal_user = User(address(0x0), 0,"null", "null", 0, "null");
+    Deal current_deal = Deal(0,"null",address(0x0),0,address(0x0),false,0,false,"null");
+
+    //初始玩家数和订单数设置为0
+    constructor() {
+        num = 0;
+        deal_num = 0;
+    }
+
+    //test智能合约用来测试是否连上了以太坊，是测试用例
+    function test() public view returns (uint n){
+        return num;
+    }
+
+
+    //insert user
+    function insert_user(string memory _name,string memory _tel,string memory _dept) public {
+        require(!isPlay[msg.sender]);
+        users.push(User(msg.sender, num,_name, _tel, 100, _dept));
+        ids.push(num);
+        userId[msg.sender] = num;
+        isPlay[msg.sender] = true;
+        num += 1;
+        //uint id = _id;
+        //emit Insert(id);
+    }
+ 
+    //insert deal
+    function insert_deal(string memory _text,uint _money,uint _time,string memory _tip) public {
+        require(isPlay[msg.sender]);
+        for (uint i = 0; i < num; ++i) {
+            if (users[i].addr == msg.sender) {
+                current_user = users[i];
+            }
+        }
+        deals.push(Deal(deal_num,_text,msg.sender,_money,address(0x0),false,_time,false,_tip));
+        deal_ids.push(deal_num);
+        deal_num += 1;
+    }
+
+    //get all deals
+    function select_all() public view returns (Deal[] memory) {
+        require(deal_num>0);
+        return deals;
+    }
+
+
+    //finish a deal
+    function finish_deal(uint _id) public{
+        require(deal_num>0);
+        require(isPlay[msg.sender]);
+
+        for (uint i = 0; i < deal_num; ++i) {
+            if (deals[i].id == _id) {
+                current_deal = deals[i];
+                require(deals[i].is_finish == false);
+                require(deals[i].is_timeout == false);
+                deals[i].receive_user = msg.sender;
+                deals[i].is_finish = true;
+            }
+        }
+
+        require(current_deal.send_user!=msg.sender);
+
+        uint deal_money = current_deal.money;
+
+        
+        for (uint i = 0; i < num; ++i) {
+            if (users[i].addr == current_deal.send_user) {
+                deal_user = users[i];
+                require(users[i].money>deal_money);
+                users[i].money = users[i].money - deal_money;
+            }
+        }
+
+
+        for (uint i = 0; i < num; ++i) {
+            if (users[i].addr == msg.sender) {
+                current_user = users[i];
+                users[i].money = users[i].money + deal_money;
+            }
+        }
+
+    }
+
+    //cancel a deal
+    function cancel_deal(uint _id) public{
+        require(deal_num>0);
+        require(isPlay[msg.sender]);
+        
+        for (uint i = 0; i < deal_num; ++i) {
+            if (deals[i].id == _id) {
+                current_deal = deals[i];
+                require(deals[i].receive_user == msg.sender);
+                require(deals[i].is_finish == true);
+                require(deals[i].is_timeout == false);
+                deals[i].receive_user = address(0x0);
+                deals[i].is_finish = false;
+            }
+        }
+
+        require(current_deal.send_user!=msg.sender);
+
+        uint deal_money = current_deal.money;
+
+        for (uint i = 0; i < num; ++i) {
+            if (users[i].addr == msg.sender) {
+                deal_user = users[i];
+                require(users[i].money>deal_money);
+                users[i].money = users[i].money - deal_money;
+            }
+        }
+
+        for (uint i = 0; i < num; ++i) {
+            if (users[i].addr == current_deal.send_user) {
+                current_user = users[i];
+                users[i].money = users[i].money + deal_money;
+            }
+        }
+
+    
+    }
+
+    //current user's information
+    function select_myself() public view returns (User memory){
+        require(num>0);
+        require(isPlay[msg.sender]);
+        for (uint i = 0; i < num; ++i) {
+            if (users[i].addr == msg.sender) {
+                return users[i];
+            }
+        }
+        return User(msg.sender, 0,"null", "null", 0, "null");
+    }
+
+    //update time limit of all deals
+    function update_dealtime(uint _time_minus_hour) public{
+        if(deal_num>0){
+            for (uint i = 0; i < deal_num; ++i) {
+                if(deals[i].is_finish == false){
+                    if(deals[i].left_hour>1){
+                        deals[i].left_hour = deals[i].left_hour - _time_minus_hour;
+                    }else{
+                        deals[i].is_timeout = true;
+                        deals[i].is_finish = true;
+                    }
+                }
+                // if(deals[i].left_hour<0){
+                //     deals[i].is_timeout = true;
+                //     deals[i].is_finish = true;
+                // }
+            }
+        }
+
+    }
+
+
+//unused
+    function select_count() public view returns (uint n){
+        return num;
+    }
+
+    function select_all_id() public view returns (uint[] memory) {
+        require(num>0);
+        return ids;
+    }
+
+    function select_id(uint _id) public view returns (User memory){
+        require(num>0);
+        require(isPlay[msg.sender]);
+        for (uint i = 0; i < num; ++i) {
+            if (users[i].id == _id) {
+                return users[i];
+            }
+        }
+        return User(msg.sender, 0,"null", "null", 0, "null");
+    }
+
+
+}
---
+++ qk修改/app.py
@@ -0,0 +1,290 @@
+import string
+
+from flask import Flask, render_template, request
+# from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+from web3_deploy import *
+# from user import User
+from flask_cors import CORS
+import json
+from flask_apscheduler import APScheduler
+
+app = Flask(__name__)
+CORS(app)
+# app.secret_key = '1234567'
+
+# login_manager = LoginManager()
+# login_manager.login_view = 'login'
+# login_manager.login_message_category = 'info'
+# login_manager.login_message = '请登录'
+# login_manager.init_app(app)
+
+WaiMai_contract = compile_and_deploy("WaiMai")
+eth.default_account = ""
+
+
+# @login_manager.user_loader
+# def load_user(user_id):
+#     curr_user = User()
+#     curr_user.id = user_id
+#
+#     return curr_user
+
+
+@app.route('/')
+def hello():
+    # answer = helloworld_.functions.say().call()
+    # return answer
+    return render_template('index.html')
+
+
+@app.route('/valid', methods=['POST'])
+def valid():
+    if request.method == 'POST':
+        # try:
+        data = request.get_data()
+        json_data = json.loads(data.decode("utf-8"))
+        address = json_data.get("Name")
+        eth.default_account = address
+        print(eth.default_account)
+        test_num = WaiMai_contract.functions.test().call()
+        print(test_num)
+        # curr_user = User()
+        # curr_user.id = address
+        # # 通过Flask-Login的login_user方法登录用户
+        # login_user(curr_user)
+        # return render_template('information.html')
+        res_list = {}
+        data = json.loads(json.dumps(res_list))
+        data['status'] = 1
+        data['msg'] = "success"
+        res = json.dumps(data, ensure_ascii=False)
+        return res
+    # except:
+    #     return "<h1>以太坊账户登录失败</h1>"
+
+
+@app.route('/information', methods=['POST'])
+# @login_required
+def information():
+    if request.method == 'POST':
+        # try:
+        # eth.default_account = current_user.get_id()
+        data = request.get_data()
+        print("data = %s" % data)
+        json_data = json.loads(data.decode("utf-8"))
+        name = json_data.get("name")
+        tel = json_data.get("tel")
+        dept = json_data.get("dept")
+        information = (name, tel, dept)
+        tx_hash = WaiMai_contract.functions.insert_user(*information).transact()
+        tx_receipt = eth.waitForTransactionReceipt(tx_hash)
+        in_json = '{"statue": 1, "msg": "add success"}'
+        return json.loads(in_json)
+        # if student is None:
+        #    return "<h1>找不到该id对应的value</h1>"
+        # student = tuple(student)
+        # return "<h1>信息完善成功</h1>"
+    # except:
+    #     return "<h1>你已完善过信息或者插入信息失败</h1>"
+
+
+@app.route('/add', methods=['POST'])
+# @login_required
+def add():
+    if request.method == 'POST':
+        # try:
+        # eth.default_account = current_user.get_id()
+        data = request.get_data()
+        print("data = %s" % data)
+        json_data = json.loads(data.decode("utf-8"))
+        name = json_data.get("name")
+        money_string = json_data.get("money")
+        time = json_data.get("num_time")
+        tip = json_data.get("tip")
+        money = int(money_string)
+        deal = (name, money, int(time), tip)
+        print(deal)
+        tx_hash = WaiMai_contract.functions.insert_deal(*deal).transact()
+        tx_receipt = eth.waitForTransactionReceipt(tx_hash)
+        in_json = '{"statue": 1, "msg": "add record success"}'
+        return json.loads(in_json)
+    # except:
+    #     return "<h1>发布信息失败</h1>"
+
+
+@app.route('/output', methods=['POST'])
+# @login_required
+def output():
+    if request.method == 'POST':
+        # eth.default_account = current_user.get_id()
+        deals = WaiMai_contract.functions.select_all().call()
+        list1 = []
+        for deal in deals:
+            print("deal is :", deal)
+            list2 = []
+            for j in range(9):
+                list2.append(deal[j])
+            list1.append(list2)
+        print("list1 is: ", list1)
+        jsonList = []
+        for list in list1:
+            aItem = {}
+            print("list is ", list)
+            aItem["id"] = list[0]
+            aItem["username"] = list[2]
+            aItem["money"] = list[3]
+            aItem["mission"] = list[1]
+            aItem["tip"] = list[8]
+            aItem["time"] = list[6]
+            jsonList.append(aItem)
+        print(jsonList)
+        jsonArr = json.dumps(jsonList, ensure_ascii=False)
+        return jsonArr
+
+
+# except:
+#     return "<h1>查看信息失败</h1>"
+
+
+@app.route('/add_deal')
+# @login_required
+def add_deal():
+    return render_template('add.html')
+
+
+@app.route('/finish_deal', methods=['POST'])
+# @login_required
+def finish_deal():
+    if request.method == 'POST':
+        # try:
+        # eth.default_account = current_user.get_id()
+        try:
+            data = request.get_data()
+            print("data = %s" % data)
+            json_data = json.loads(data.decode("utf-8"))
+            name = json_data.get("Name")
+            deal_id = int(name)
+            print(eth.default_account)
+            print(deal_id)
+            tx_hash = WaiMai_contract.functions.finish_deal(deal_id).transact()
+            tx_receipt = eth.waitForTransactionReceipt(tx_hash)
+            in_json = '{"statue": 1, "msg": "success"}'
+            return json.loads(in_json)
+        except:
+            in_json = '{"statue": 0, "msg": "failed"}'
+            return json.loads(in_json)
+    # except:
+    #   return "<h1>接单失败</h1>"
+
+
+# 618 edited by qk!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# 取消订单和上面finish订单对应
+@app.route('/cancel_deal', methods=['POST'])
+# @login_required
+def cancel_deal():
+    if request.method == 'POST':
+        # try:
+        # eth.default_account = current_user.get_id()
+        try:
+            data = request.get_data()
+            print("data = %s" % data)
+            json_data = json.loads(data.decode("utf-8"))
+            name = json_data.get("Name")
+            deal_id = int(name)
+            print(eth.default_account)
+            print(deal_id)
+            tx_hash = WaiMai_contract.functions.cancel_deal(deal_id).transact()
+            tx_receipt = eth.waitForTransactionReceipt(tx_hash)
+            in_json = '{"statue": 1, "msg": "success"}'
+            return json.loads(in_json)
+        except:
+            in_json = '{"statue": 0, "msg": "failed"}'
+            return json.loads(in_json)
+    # except:
+    #   return "<h1>接单失败</h1>"
+
+
+# 618 edited by qk!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# 返回当前账户个人信息
+@app.route('/myself', methods=['POST'])
+# @login_required
+def myself():
+    if request.method == 'POST':
+        # eth.default_account = current_user.get_id()
+        my = WaiMai_contract.functions.select_myself().call()
+        print(my)
+        jsonList = []
+        aItem = {}
+        aItem["addr"] = my[0]
+        aItem["id"] = my[1]
+        aItem["name"] = my[2]
+        aItem["tel"] = my[3]
+        aItem["money"] = my[4]
+        aItem["dept"] = my[5]
+        jsonList.append(aItem)
+        print(jsonList)
+        jsonArr = json.dumps(jsonList, ensure_ascii=False)
+        return jsonArr
+
+
+# except:
+#     return "<h1>查看信息失败</h1>"
+
+
+# 618 edited by qk!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#
+# 配置自动任务的类
+class Config(object):
+    JOBS = [
+        {
+            'id': 'job1',
+            'func': '__main__:update',
+            'trigger': 'interval',
+            'seconds': 10,
+
+        },
+    ]
+
+
+def update():
+    # try:
+        now_account = eth.default_account
+        eth.default_account = eth.accounts[0]
+        print("starting update!")
+        tx_hash = WaiMai_contract.functions.update_dealtime(1).transact()
+        tx_receipt = eth.waitForTransactionReceipt(tx_hash)
+        print("update success!")
+        eth.default_account = now_account;
+    # except:
+    #     print("update failed!")
+
+
+#unused
+@app.route('/query/<int:key>')
+# @login_required
+def query_process(key):
+    key_int = int(key)
+    # eth.default_account = current_user.get_id()
+
+    try:
+        student = WaiMai_contract.functions.select_id(key_int).call()
+        if student is None:
+            return "<h1>找不到该id对应的value</h1>"
+        student = tuple(student)
+        return render_template('answer.html', Value=student)
+    except:
+        return "<h1>query error</h1>"
+
+
+# @app.route('/logout')
+# # @login_required
+# def logout():
+#     logout_user()
+#     return 'Logged out successfully!'
+
+if __name__ == '__main__':
+    app.config.from_object(Config())
+    scheduler = APScheduler()
+    scheduler.init_app(app)
+    scheduler.start()
+    app.run()
